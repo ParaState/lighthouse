d364109bf342d76033c36571a1f21d6ec4e8bba4
jianlinjiang <jianlinjiang@whu.edu.cn>
2025-01-05 15:43:35 +0000

    change channel size

--- validator_client/safestake_operator/src/lib.rs
+++ validator_client/safestake_operator/src/lib.rs
@@ -24,7 +24,7 @@ use tonic::transport::Channel;
 use types::{graffiti::GraffitiString, AttestationData, PublicKey};
 use types::{Hash256, Signature};
 
-pub const CHANNEL_SIZE: usize = 12;
+pub const CHANNEL_SIZE: usize = 32;
 
 lazy_static! {
     pub static ref THRESHOLD_MAP: HashMap<u64, u64> = {
